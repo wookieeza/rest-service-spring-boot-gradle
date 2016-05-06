bd54190193116b76aaf97fccd606c20d3ecf037c
Debbie Wood <wood.debbie@gmail.com>
2016-05-06 10:23:23 +0100

    initial

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,16 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by debbiewood on 06/05/2016.
+ */
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String [] args){
+        SpringApplication.run(Application.class,args);
+    }
+}
---
+++ src/main/java/hello/Greeting.java
@@ -0,0 +1,24 @@
+package hello;
+
+/**
+ * Created by debbiewood on 06/05/2016.
+ */
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
